35f0dff8ebbd1ec34152932fc77686c8e9c15134
zjmckerrow <51459617+zjmckerrow@users.noreply.github.com>
2020-02-07 11:01:41 -0600

    changed paths in config.py for training

--- transferlearning/pyimagesearch/config.py
+++ transferlearning/pyimagesearch/config.py
@@ -2,7 +2,7 @@
 import os
 
 # initialize the path to the *original* input directory of images
-ORIG_INPUT_DATASET = "transferlearning/Food-5K"
+ORIG_INPUT_DATASET = "transferlearning/plastics"
 
 # initialize the base path to the *new* directory that will contain
 # our images after computing the training and testing split
@@ -15,7 +15,7 @@ TEST = "evaluation"
 VAL = "validation"
 
 # initialize the list of class label names
-CLASSES = ["non_food", "food"]
+CLASSES = ["bead", "fiber"]
 
 # set the batch size
 BATCH_SIZE = 32
@@ -23,7 +23,7 @@ BATCH_SIZE = 32
 # initialize the label encoder file path and the output directory to
 # where the extracted features (in CSV file format) will be stored
 LE_PATH = os.path.sep.join(["transferlearning/output", "le.cpickle"])
-BASE_CSV_PATH = "output"
+BASE_CSV_PATH = "transferlearning/output"
 
 # set the path to the serialized model after training
-MODEL_PATH = os.path.sep.join(["output", "model.cpickle"])
+MODEL_PATH = os.path.sep.join(["transferlearning/output", "model.cpickle"])
