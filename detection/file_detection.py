a5854c3ef66851a66995f6e16f3ce1ca743feb25
zjmckerrow <zjmckerrow@crimson.ua.edu>
2020-02-12 09:46:54 -0600

    worked with pickle

--- detection/file_detection.py
+++ detection/file_detection.py
@@ -2,12 +2,14 @@
 # python detection/file_detection.py --prototxt models/MobileNetSSD_deploy.prototxt --model models/MobileNetSSD_deploy.caffemodel --input videos/people2.avi --output videos/output.avi
 
 # import the necessary packages
+from keras.models import load_model
 from pyimagesearch.centroidtracker import CentroidTracker
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
+import pickle
 import time
 from cv2 import cv2
 
@@ -44,6 +46,7 @@ ct = CentroidTracker()
 
 # load our serialized model from disk
 print("[INFO] loading model...")
+model = pickle.loads(open("transferlearning/output/model.cpickle", "rb").read())
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # initialize the video stream, allow the cammera sensor to warmup,
@@ -78,7 +81,7 @@ while(vs.isOpened()):
 	
     if ret == True:
         
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (224, 224)), 0.007843, (224, 224), 127.5)
         
 	    # pass the blob through the network and obtain the detections and
 	    # predictions
